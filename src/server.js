ebf413f60d8f34cf3b5656ed45e3df09a1f975b9
Navin Kumar <navin1551@gmail.com>
2019-06-05 21:21:49 -0700

    first commit

--- src/server.js
+++ src/server.js
@@ -4,3 +4,18 @@ const { PORT } = require("./config");
 app.listen(PORT, () => {
   console.log(`Server listening at http://localhost:${PORT}`);
 });
+
+/*
+const express = require("express");
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.get("/api/*", (req, res) => {
+  res.json({ ok: true });
+});
+
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
+
+module.exports = { app };
+*/
