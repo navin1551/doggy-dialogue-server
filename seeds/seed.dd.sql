6770c6f1033796bf9d7de0c02a2f10314afd9607
Navin Kumar <navin1551@gmail.com>
2019-06-11 12:17:03 -0700

    implemented bcrypt

--- seeds/seed.dd.sql
+++ seeds/seed.dd.sql
@@ -1,3 +1,12 @@
+BEGIN;
+
+TRUNCATE
+  forums,
+  users,
+  posts,
+  replies
+  RESTART IDENTITY CASCADE;
+
 INSERT INTO forums (title)
 VALUES
   ('Introductions'),
@@ -14,14 +23,14 @@ VALUES
   ('Dog Memorials'),
   ('Dog Breeds');
 
-INSERT INTO users (full_name, password)
+INSERT INTO users (user_name, password)
 VALUES
-('User2', 'password2'),
-('User3', 'password3'),
-('User4', 'password4'),
-('User5', 'password5'),
-('User6', 'password6'),
-('User7', 'password7');
+('User2', '$2a$12$fqdA45z8bGKsaxnE3KyjVO5HScjZEu4dZmJorlhv1Vpjj2F0ubHVG'),
+('User3', '$2a$12$4mSe8R3vJen.KTLpdJCIGeEKidqPyebgT4hWQAFz5204UGEnZrfcq'),
+('User4', '$2a$12$hHg21Wx450d.t7e4bJMw4ub83VfAzvO7aqOsR4DCBOIXsaTeIHW82'),
+('User5', '$2a$12$oNiKytzw38lFPidS8N.wV.hwHuLY3cjhNk87uRZmiKsEo3BFMD74C'),
+('User6', '$2a$12$WvhRt6CHA3lrzOGHB64/jOAjFNT2wTvJ5bL2tK4MzaqD5qxS/NClG'),
+('User7', '$2a$12$kcdyiJHv8xVJ10r0e46dIeK8U82oqugHWZRzmgCshCsNWt5LeZDem');
 
 
 
@@ -43,5 +52,5 @@ VALUES
 ('Reply5', '5', '5'),
 ('Reply6', '6', '6');
 
-
+COMMIT;
 
